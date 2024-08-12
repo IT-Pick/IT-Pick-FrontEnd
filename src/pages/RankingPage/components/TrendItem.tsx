e1397ba4872b2862c61b2e53c1bc47019138cfb0
yskim6772 <yskim6772@gmail.com>
2024-08-12 17:40:28 +0900

    Feat: 랭킹 페이지 날짜 표시 구현

--- src/pages/RankingPage/components/TrendItem.tsx
+++ src/pages/RankingPage/components/TrendItem.tsx
@@ -11,14 +11,14 @@ const TrendItem: React.FC<TrendItemProps> = ({ rank, name, tags }) => {
   const rankColor = rank === 1 || rank === 2 || rank === 3 ? 'text-point500' : 'text-gray3';
 
   return (
-    <div className="flex flex-col justify-between p-4 border-b">
+    <div className="flex flex-col font-pretendard justify-between p-4 border-b">
       <div className="flex items-center">
         <span className={`text-lg font-bold ${rankColor}`}>{rank}</span>
         <span className="ml-4 text-lg">{name}</span>
       </div>
       <div className=" space-x-2">
         {tags.map((tag, index) => (
-          <span key={index} className="ml-[22px] px-2 py-1 bg-purple-100 rounded-2xl text-xs text-violet-700 font-medium">
+          <span key={index} className="ml-[22px] px-2 py-1 bg-point100 rounded-2xl text-xs text-point500 font-medium">
             {tag}
           </span>
         ))}
