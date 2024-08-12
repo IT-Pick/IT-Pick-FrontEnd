7c2fa092ca6100d240c6a7bb2d0c99a5297faba4
yskim6772 <yskim6772@gmail.com>
2024-08-12 16:12:01 +0900

    Feat: 랭킹 페이지 순위 컬러 수정

--- src/pages/RankingPage/components/TrendItem.tsx
+++ src/pages/RankingPage/components/TrendItem.tsx
@@ -7,10 +7,13 @@ interface TrendItemProps {
 }
 
 const TrendItem: React.FC<TrendItemProps> = ({ rank, name, tags }) => {
+
+  const rankColor = rank === 1 || rank === 2 || rank === 3 ? 'text-point500' : 'text-gray3';
+
   return (
     <div className="flex flex-col justify-between p-4 border-b">
       <div className="flex items-center">
-        <span className="text-lg font-bold text-violet-700">{rank}</span>
+        <span className={`text-lg font-bold ${rankColor}`}>{rank}</span>
         <span className="ml-4 text-lg">{name}</span>
       </div>
       <div className=" space-x-2">
