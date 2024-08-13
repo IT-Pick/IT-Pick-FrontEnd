7a613f971bfbba34bfcdfd5aba9c2b3f80ec47d8
yskim6772 <yskim6772@gmail.com>
2024-08-14 00:27:31 +0900

    Chore: fix ranking page css

--- src/pages/RankingPage/components/TrendItem.tsx
+++ src/pages/RankingPage/components/TrendItem.tsx
@@ -11,17 +11,21 @@ const TrendItem: React.FC<TrendItemProps> = ({ rank, name, tags }) => {
   const rankColor = rank === 1 || rank === 2 || rank === 3 ? 'text-point500' : 'text-gray3';
 
   return (
-    <div className="flex flex-col font-pretendard justify-between pt-2 pb-3 border-b">
+    <div className="flex flex-col font-pretendard justify-between py-3 border-b">
       <div className="flex items-center">
-        <span className={`text-[20px] font-bold ${rankColor}`}>{rank}</span>
-        <span className="ml-4 text-[16px]">{name}</span>
-      </div>
-      <div className=" space-x-2">
-        {tags.map((tag, index) => (
-          <span key={index} className="ml-[22px] px-2 py-1 bg-point100 rounded-2xl text-xs text-point500 font-medium">
-            {tag}
-          </span>
-        ))}
+        <div className="flex flex-col justify-center">
+          <span className={`text-[20px] font-bold ${rankColor}`}>{rank}</span>
+        </div>
+        <div className="ml-4">
+          <span className="text-[16px] font-medium">{name}</span>
+          <div className="mt-1 space-x-1">
+            {tags.map((tag, index) => (
+              <span key={index} className="px-2 py-1 bg-point100 rounded-2xl text-xs text-point500 font-medium">
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
