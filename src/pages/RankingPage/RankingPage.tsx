892fc130a7a71d6288e6d117cd6a701e51c7c2b1
yskim6772 <yskim6772@gmail.com>
2024-08-13 14:49:44 +0900

    Chore: add component

--- src/pages/RankingPage/RankingPage.tsx
+++ src/pages/RankingPage/RankingPage.tsx
@@ -3,8 +3,7 @@ import CategorySlider from './components/CategorySlider';
 import TrendList from './components/TrendList';
 import { useTrendStore } from '../../store/trendStore';
 import MenuSelector from './components/MenuSelector';
-import leftArrow from '../../assets/images/24x24/ico_left_arrow_gray2.svg';
-import rightArrow from '../../assets/images/24x24/ico_right_arrow_gray2.svg';
+import DateDisplay from './components/DateDisplay';
 
 const RankingPage: React.FC = () => {
   const { menuType, setMenuType, date, setDate, fetchTrends } = useTrendStore();
@@ -79,27 +78,14 @@ const RankingPage: React.FC = () => {
     <div className="bg-background w-[390px] mx-auto font-pretendard">
       <MenuSelector menuType={menuType} setMenuType={setMenuType} />
       <CategorySlider />
-      {menuType !== 'realTime' && (
-        <div className="flex justify-between items-center h-[36px] mt-4 mx-5">
-          <button onClick={() => handleDateChange('prev')}>
-            <img src={leftArrow} alt="left arrow" />
-          </button>
-          <span className="font-pretendard font-semibold text-[16px] text-black">
-          {menuType === 'daily'
-              ? formatDate(new Date(date))
-              : getWeeklyDateRange(date)}
-          </span>
-          <button onClick={() => handleDateChange('next')}>
-            <img src={rightArrow} alt="right arrow" />
-          </button>
-        </div>
-      )}
-      {menuType === 'realTime' && (
-        <div className="flex flex-col items-center h-[36px] mt-4">
-          <span className="font-semibold text-[16px] text-black">{formatDate(new Date())}</span>
-          <span className="font-normal text-[12px] text-gray3">{currentTime}</span>
-        </div>
-      )}
+      <DateDisplay
+        menuType={menuType}
+        date={date}
+        currentTime={currentTime}
+        handleDateChange={handleDateChange}
+        formatDate={formatDate}
+        getWeeklyDateRange={getWeeklyDateRange}
+      />
       <main className="container mx-auto p-4">
         <TrendList />
       </main>
