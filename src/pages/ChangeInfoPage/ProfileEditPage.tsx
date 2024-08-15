e2eb81707149ec9c351bab0f00bcbf1c8b47de04
yskim6772 <yskim6772@gmail.com>
2024-08-16 02:59:10 +0900

    Feat: upload image from local gallery

--- src/pages/ChangeInfoPage/ProfileEditPage.tsx
+++ src/pages/ChangeInfoPage/ProfileEditPage.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import profile from '../../assets/images/ico_profile_edit.svg';
+import profile from '../../assets/images/bear.png';
 import DeleteAccoutModal from '../../components/Modal/DeleteAccoutModal';
 
 const ProfileEditPage: React.FC = () => {
     const [name, setName] = useState('김잇픽');
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+    const [profileImage, setProfileImage] = useState(profile);
     const navigate = useNavigate();
 
     const handleChangePasswordClick = () => {
@@ -35,7 +36,18 @@ const ProfileEditPage: React.FC = () => {
 
     const handleInterest = () => {
         navigate('/interest');
-    }
+    };
+
+    const handleProfileImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setProfileImage(reader.result as string);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
 
     return (
         <div className="w-[390px] h-screen flex flex-col items-center mx-auto bg-background">
@@ -44,11 +56,18 @@ const ProfileEditPage: React.FC = () => {
                 <button className="mr-6 font-pretendard font-medium text-[14px] text-point400">완료</button>
             </header>
             <div className="flex flex-col items-center mt-5 text-center">
-                <button>
-                    <img src={profile} alt="profile_image" className="w-20 h-20" />
-                </button>
+                <label htmlFor="profileImageInput" className="cursor-pointer">
+                    <img src={profileImage} alt="profile_image" className="w-20 h-20 rounded-full object-cover" />
+                </label>
+                <input
+                    id="profileImageInput"
+                    type="file"
+                    accept="image/*"
+                    style={{ display: 'none' }}
+                    onChange={handleProfileImageChange}
+                />
                 <textarea
-                    className="w-[352px] h-[54px] pt-[12px] pb-[12px] pl-[20px] mt-6 bg-gray1 rounded-[8px] focus:outline-none text-black placeholder-gray3 text-[18px] font-pretendard font-medium resize-none"
+                    className="w-[352px] h-[54px] pt-[12px] pb-[12px] px-[20px] mt-6 bg-gray1 rounded-[8px] focus:outline-none text-black placeholder-gray3 text-[18px] font-pretendard font-medium resize-none"
                     contentEditable
                     suppressContentEditableWarning
                     onChange={handleInput}
