57ce411e44211abe951f4d5bbb16820efcd9a108
yskim6772 <yskim6772@gmail.com>
2024-08-17 01:09:47 +0900

    Fix: restore code

--- src/apis/loginUser.ts
+++ src/apis/loginUser.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 export const loginUser = async (email: string, password: string) => {
   try {
-    const response = await axios.post('https://itpick.store/auth/login', {
+    const response = await axios.post('/auth/login', {
       email,  // encodeURIComponent 제거
       password,  // encodeURIComponent 제거
-    }, { withCredentials: true });
+    });
 
     return response.data;
   } catch (error) {
