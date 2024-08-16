e12ee8f18f692a5dd5443d11dfe0ecad4a32ab44
yaho <rlatjwls2923@gmail.com>
2024-08-16 12:10:27 +0900

    feat: email verification complete

--- src/apis/sendEmailVerification.ts
+++ src/apis/sendEmailVerification.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 export const sendEmailVerification = async (email: string) => {
-    const response = await axios.post(`/auth/emails/verification-requests`, {
-        params: {
-            email: encodeURIComponent(email), // Request body로 email을 전송
-        }
-
-        }, 
+    const response = await axios.post(
+        `/auth/emails/verification-requests`,
+        null,
         {
+            params: {
+                email: email,  // Query parameter로 email 전송
+            },
             headers: {
-                'Authorization': 'Bearer <token>',  // Authorization 헤더에 토큰 추가
-                'Content-Type': 'application/json', // Content-Type 설정
-            }
+                'Content-Type': 'application/json',
+            },
         }
     );
+
     return response.data;
-}
+};
