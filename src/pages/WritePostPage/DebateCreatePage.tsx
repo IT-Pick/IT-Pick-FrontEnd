4eda25463c71a7acf7ad06bd3a0bc4e6744c3b2a
clice lee <cliceleee@gmail.com>
2024-08-17 12:03:26 +0900

    design: 투표 컴포넌트 사진 추가 svg 추가

--- src/pages/WritePostPage/DebateCreatePage.tsx
+++ src/pages/WritePostPage/DebateCreatePage.tsx
@@ -1,96 +1,96 @@
-import React, { useState, useEffect } from 'react';
-import DebateIconBar from './components/DebateIconBar';
-import VoteCreationForm from '../../components/Vote/VoteCreationForm';
-import DialogModal from '@components/Modal/DialogModal';
-import Modal from 'react-modal';
+// import React, { useState, useEffect } from 'react';
+// import DebateIconBar from './components/DebateIconBar';
+// import VoteCreationForm from '../../components/Vote/VoteCreationForm';
+// import DialogModal from '@components/Modal/DialogModal';
+// import Modal from 'react-modal';
 
-// Modal 설정: root element를 지정해야 합니다.
-Modal.setAppElement('#root'); // root id는 프로젝트의 root element id와 맞춰야 합니다.
+// // Modal 설정: root element를 지정해야 합니다.
+// Modal.setAppElement('#root'); // root id는 프로젝트의 root element id와 맞춰야 합니다.
 
 
 
-const DebateCreatePage: React.FC = () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
-  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
-  const [isVoteModalOpen, setIsVoteModalOpen] = useState(false);
+// const DebateCreatePage: React.FC = () => {
+//   const [title, setTitle] = useState('');
+//   const [content, setContent] = useState('');
+//   const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
+//   const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+//   const [isVoteModalOpen, setIsVoteModalOpen] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      const currentViewportHeight = visualViewport.height;
-      setViewportHeight(currentViewportHeight);
+//   useEffect(() => {
+//     const handleResize = () => {
+//       const currentViewportHeight = visualViewport.height;
+//       setViewportHeight(currentViewportHeight);
 
-      if (window.visualViewport.height < window.innerHeight) {
-        setIsKeyboardVisible(true);
-      } else {
-        setIsKeyboardVisible(false);
-      }
-    };
+//       if (window.visualViewport.height < window.innerHeight) {
+//         setIsKeyboardVisible(true);
+//       } else {
+//         setIsKeyboardVisible(false);
+//       }
+//     };
 
-    window.visualViewport.addEventListener('resize', handleResize);
-    handleResize(); // 초기화
-    return () => {
-      window.visualViewport.removeEventListener('resize', handleResize);
-    };
-  }, []);
+//     window.visualViewport.addEventListener('resize', handleResize);
+//     handleResize(); // 초기화
+//     return () => {
+//       window.visualViewport.removeEventListener('resize', handleResize);
+//     };
+//   }, []);
 
-  return (
-    <div className="w-[390px] h-screen mx-auto flex flex-col items-center justify-between bg-background">
-      <div className="w-full h-full flex flex-col">
-        <div className="flex justify-between items-center mb-6 py-4 px-6 bg-white">
-          <div className="font-pretendard font-bold text-lg">
-            <span className="text-point500">#김현주 열애설</span>
-            <span className="text-black"> 토론 만들기</span>
-          </div>
-          <button 
-            className="text-point400 font-pretendard font-medium text-[14px]"
-            onClick={() => setIsVoteModalOpen(true)}
-          >
-            투표 만들기
-          </button>
-        </div>
-        <input 
-          type="text" 
-          placeholder="제목을 입력해주세요."
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="w-[335px] px-5 font-pretendard font-bold text-[18px] text-[#2E333B] placeholder-gray3 border-none focus:outline-none bg-background"
-        />
-        <hr className="w-[350px] border-t border-gray1 my-[20px]" />
-        <textarea
-          placeholder="자유롭게 이야기해 보세요."
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          className="w-[335px] flex-grow px-5 font-pretendard font-medium text-[16px] text-gray5 placeholder-gray3 border-none focus:outline-none resize-none bg-background"
-        />
-      </div>
-      <div className={`w-[390px] flex justify-center py-3 bg-white ${isKeyboardVisible ? 'fixed bottom-0' : 'absolute bottom-0'}`}
-        style={{ bottom: isKeyboardVisible ? `${window.innerHeight - viewportHeight}px` : '0' }}>
-        <DebateIconBar />
-      </div>
+//   return (
+//     <div className="w-[390px] h-screen mx-auto flex flex-col items-center justify-between bg-background">
+//       <div className="w-full h-full flex flex-col">
+//         <div className="flex justify-between items-center mb-6 py-4 px-6 bg-white">
+//           <div className="font-pretendard font-bold text-lg">
+//             <span className="text-point500">#김현주 열애설</span>
+//             <span className="text-black"> 토론 만들기</span>
+//           </div>
+//           <button 
+//             className="text-point400 font-pretendard font-medium text-[14px]"
+//             onClick={() => setIsVoteModalOpen(true)}
+//           >
+//             투표 만들기
+//           </button>
+//         </div>
+//         <input 
+//           type="text" 
+//           placeholder="제목을 입력해주세요."
+//           value={title}
+//           onChange={(e) => setTitle(e.target.value)}
+//           className="w-[335px] px-5 font-pretendard font-bold text-[18px] text-[#2E333B] placeholder-gray3 border-none focus:outline-none bg-background"
+//         />
+//         <hr className="w-[350px] border-t border-gray1 my-[20px]" />
+//         <textarea
+//           placeholder="자유롭게 이야기해 보세요."
+//           value={content}
+//           onChange={(e) => setContent(e.target.value)}
+//           className="w-[335px] flex-grow px-5 font-pretendard font-medium text-[16px] text-gray5 placeholder-gray3 border-none focus:outline-none resize-none bg-background"
+//         />
+//       </div>
+//       <div className={`w-[390px] flex justify-center py-3 bg-white ${isKeyboardVisible ? 'fixed bottom-0' : 'absolute bottom-0'}`}
+//         style={{ bottom: isKeyboardVisible ? `${window.innerHeight - viewportHeight}px` : '0' }}>
+//         <DebateIconBar />
+//       </div>
 
-      {isVoteModalOpen && (
-         <DialogModal
-         isOpen={isVoteModalOpen}
-         onRequestClose={() => setIsVoteModalOpen(false)}
-         onConfirm={() => setIsVoteModalOpen(false)}
-         icon="투표 아이콘 경로"
-         title="투표 만들기"
-         message={(
-           <VoteCreationForm onSubmit={(options, isMultipleChoice) => {
-             // 투표 생성을 처리하고, 모달을 닫음
-             console.log('투표 생성:', options, isMultipleChoice);
-             setIsVoteModalOpen(false);
-           }} />
-         )}
-         confirmText="확인"
-         contentLabel="Vote Modal"
-         isVoteModal={true} // 투표 모달이기 때문에 true로 설정
-       />
-      )}
-    </div>
-  );
-};
+//       {isVoteModalOpen && (
+//          <DialogModal
+//          isOpen={isVoteModalOpen}
+//          onRequestClose={() => setIsVoteModalOpen(false)}
+//          onConfirm={() => setIsVoteModalOpen(false)}
+//          icon="투표 아이콘 경로"
+//          title="투표 만들기"
+//          message={(
+//            <VoteCreationForm onSubmit={(options, isMultipleChoice) => {
+//              // 투표 생성을 처리하고, 모달을 닫음
+//              console.log('투표 생성:', options, isMultipleChoice);
+//              setIsVoteModalOpen(false);
+//            }} />
+//          )}
+//          confirmText="확인"
+//          contentLabel="Vote Modal"
+//          isVoteModal={true} // 투표 모달이기 때문에 true로 설정
+//        />
+//       )}
+//     </div>
+//   );
+// };
 
-export default DebateCreatePage;
+// export default DebateCreatePage;
--- src/pages/WritePostPage/MakingVote.tsx
+++ src/pages/WritePostPage/MakingVote.tsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
+import ico_vote_add_photo from '../../assets/images/etc/ico_vote_add_photo.svg'; // SVG 파일을 올바르게 import
 
 // 항목 컴포넌트 (ItemInput)
-const ItemInput: React.FC<{ id: number }> = ({ id }) => {
+const ItemInput: React.FC<{ id: number; onRemove: (id: number) => void; canRemove: boolean }> = ({ id, onRemove, canRemove }) => {
   return (
     <div className="w-[350px] h-[47px] flex-col justify-center items-start gap-3 inline-flex">
       <div className="w-[350px] h-[35px] justify-start items-center gap-3 inline-flex">
-        <div className="w-[18px] h-[18px] relative" />
+        <button
+          className={`w-[18px] h-[18px] relative ${canRemove ? 'text-red-500' : 'text-gray-300 cursor-not-allowed'}`}
+          onClick={() => canRemove && onRemove(id)}
+          disabled={!canRemove}
+          aria-label="항목 삭제"
+        >
+          ✕
+        </button>
         <div className="h-[35px] justify-between items-center flex">
-          <div className="text-[#ced5db] text-base font-medium font-['Pretendard']">
-            항목 입력 {id}
-          </div>
+          <div className="text-[#ced5db] text-base font-medium font-['Pretendard']">항목 입력</div>
           <div className="w-[35px] h-[35px] relative">
-            <div className="w-[35px] h-[35px] left-0 top-0 absolute bg-[#edf0f3] rounded" />
+            <img src={ico_vote_add_photo} alt="사진 추가" className="w-[35px] h-[35px]" /> 
             <div className="w-[23px] h-[23px] left-[6px] top-[6px] absolute opacity-80" />
           </div>
         </div>
@@ -23,41 +29,47 @@ const ItemInput: React.FC<{ id: number }> = ({ id }) => {
 
 // 투표 만들기 컴포넌트 (MakingVote)
 const MakingVote: React.FC = () => {
-  // 항목 리스트를 관리하는 상태
-  const [items, setItems] = useState<number[]>([]);
+  // 항목 리스트를 관리하는 상태 (초기값으로 두 개의 항목)
+  const [items, setItems] = useState<number[]>([1, 2]);
 
   // 항목 추가 함수
   const addItem = () => {
     setItems((prevItems) => [...prevItems, prevItems.length + 1]);
   };
 
+  // 항목 삭제 함수
+  const removeItem = (id: number) => {
+    if (items.length > 2) {
+      setItems((prevItems) => prevItems.filter((item) => item !== id));
+    }
+  };
+
   return (
     <div className="w-[390px] h-screen mx-auto flex flex-col items-center justify-between bg-background">
       <div className="w-full h-full flex flex-col">
         <div className="flex justify-between items-center mb-6 py-4 px-6 bg-white">
-          <div className="flex flex-row justify-between items-center ">
-            <span className="text-point500 text-lg font-bold font-pretendard">
-              투표 만들기
-            </span>
-            <span className="text-gray3 text-sm font-medium font-pretendard">
-              완료
-            </span>
+          <div className="flex flex-row justify-between items-center">
+            <span className="text-point500 text-lg font-bold font-pretendard">투표 만들기</span>
+            <span className="text-gray3 text-sm font-medium font-pretendard">완료</span>
           </div>
         </div>
 
         {/* 여기에 항목이 추가됨 */}
         <div className="flex flex-col items-center">
           {items.map((item) => (
-            <ItemInput key={item} id={item} />
+            <ItemInput
+              key={item}
+              id={item}
+              onRemove={removeItem}
+              canRemove={items.length > 2} // 항목이 2개 이상일 때만 삭제 가능
+            />
           ))}
         </div>
 
         {/* 항목 추가 버튼 */}
         <div className="w-[350px] h-[52px] relative mt-4" onClick={addItem}>
           <div className="w-[350px] h-[52px] left-0 top-0 absolute bg-[#7620e4] rounded-lg cursor-pointer flex justify-center items-center">
-            <div className="text-center text-white text-lg font-semibold font-['Pretendard']">
-              항목 추가
-            </div>
+            <div className="text-center text-white text-lg font-semibold font-['Pretendard']">항목 추가</div>
           </div>
         </div>
       </div>
