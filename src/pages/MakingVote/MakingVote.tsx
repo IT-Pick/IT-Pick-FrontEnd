653608f5f194b74bc522ff4499b5ebe3b93df7bc
clice lee <cliceleee@gmail.com>
2024-08-17 12:50:59 +0900

    feat: 항목 추가 버튼 컴포넌트 활성 비활성 기능 추가

--- src/pages/MakingVote/MakingVote.tsx
+++ src/pages/MakingVote/MakingVote.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import ico_vote_add_photo from '../../assets/images/etc/ico_vote_add_photo.svg'; // SVG 파일을 올바르게 import
 import ItemInput from './component/ItemInput';
 
 // 투표 만들기 컴포넌트 (MakingVote)
 const MakingVote: React.FC = () => {
-  // 항목 리스트를 관리하는 상태 (초기값으로 두 개의 항목)
   const [items, setItems] = useState<number[]>([1, 2]);
 
   // 항목 추가 함수
   const addItem = () => {
-    setItems((prevItems) => [...prevItems, prevItems.length + 1]);
+    if (items.length < 5) {
+      setItems((prevItems) => [...prevItems, prevItems.length + 1]);
+    }
   };
 
   // 항목 삭제 함수
@@ -21,7 +21,7 @@ const MakingVote: React.FC = () => {
 
   return (
     <div className="w-[390px] h-screen mx-auto flex flex-col items-center justify-between bg-background">
-      <div className="w-full h-full flex flex-col">
+      <div className="w-full h-full flex flex-col jusify-between">
         <div className="flex justify-between items-center mb-6 py-4 px-6 bg-white">
           <div className="flex flex-row justify-between items-center">
             <span className="text-point500 text-lg font-bold font-pretendard">투표 만들기</span>
@@ -29,8 +29,7 @@ const MakingVote: React.FC = () => {
           </div>
         </div>
 
-        {/* 여기에 항목이 추가됨 */}
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col justify-between items-center">
           {items.map((item) => (
             <ItemInput
               key={item}
@@ -42,10 +41,16 @@ const MakingVote: React.FC = () => {
         </div>
 
         {/* 항목 추가 버튼 */}
-        <div className="w-[350px] h-[52px] relative mt-4" onClick={addItem}>
-          <div className="w-[350px] h-[52px] left-0 top-0 absolute bg-[#7620e4] rounded-lg cursor-pointer flex justify-center items-center">
-            <div className="text-center text-white text-lg font-semibold font-['Pretendard']">항목 추가</div>
-          </div>
+        <div
+          className={`w-[350px] h-[52px] relative mt-4 rounded-lg flex justify-center items-center ${
+            items.length >= 5 ? 'bg-gray-300 cursor-not-allowed' : 'bg-[#7620e4] cursor-pointer'
+          }`}
+          onClick={addItem}
+          style={{ pointerEvents: items.length >= 5 ? 'none' : 'auto' }}
+        >
+            <div className={`text-center text-lg font-semibold font-pretendard text-white`}>
+              항목 추가
+            </div>
         </div>
       </div>
     </div>
