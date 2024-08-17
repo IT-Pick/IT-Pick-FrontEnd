73799e6d30f82c45a0f9d772cf9bb5a5317eb73c
yaho <rlatjwls2923@gmail.com>
2024-08-17 19:46:39 +0900

    feat: 로그아웃  api 연결 & 로그인 api 수정

---
+++ src/apis/logoutUser.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const logoutUser = async () => {
+    try {
+        const token = localStorage.getItem('refreshToken');
+        const response = await axios.patch(
+            '/auth/logout',
+            null,
+            {
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.log("로그아웃 실패:", error);
+    }
+};
