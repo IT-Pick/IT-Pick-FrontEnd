406a0315c79a4269a5b66bb54536fb950cf54a11
yskim6772 <yskim6772@gmail.com>
2024-08-18 19:25:25 +0900

    Feat: fix search page popular searches time

--- src/pages/SearchPage/components/PopularSearches.tsx
+++ src/pages/SearchPage/components/PopularSearches.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface PopularSearchesProps {
   searches: string[];
 }
 
 const PopularSearches: React.FC<PopularSearchesProps> = ({ searches }) => {
+  const [currentTime, setCurrentTime] = useState('');
+
+  const updateCurrentTime = () => {
+    const now = new Date();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hour = String(now.getHours()).padStart(2, '0');
+    
+    const formattedTime = `${month}.${day} ${hour}:00 기준`;
+    setCurrentTime(formattedTime);
+  };
+
+  useEffect(() => {
+    updateCurrentTime();
+  }, []);
+
   const halfLength = Math.ceil(searches.length / 2);
   const firstHalf = searches.slice(0, halfLength);
   const secondHalf = searches.slice(halfLength);
@@ -14,7 +30,7 @@ const PopularSearches: React.FC<PopularSearchesProps> = ({ searches }) => {
       <div className="flex justify-between items-center">
         <label className="block font-pretendard font-bold text-[14px] text-gray3 ml-8">통합 인기 검색어 순위</label>
         <span 
-          className="text-gray2 mr-8 font-pretendard font-medium text-[14px]">06.13 22:00 기준
+          className="text-gray2 mr-8 font-pretendard font-medium text-[14px]">{currentTime}
         </span>
       </div>
       <div className="grid grid-cols-2 gap-x-8 ml-8 mr-8">
