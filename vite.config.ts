7d7b59ec2ac4ec497fd125909ace48625553f55f
clice lee <cliceleee@gmail.com>
2024-08-18 13:48:19 +0900

    chore: 인증서 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    mkcert({
+      force: false,
+    })
+  
+  
+  ],
   resolve: {
     alias: {
       '@images': '/src/assets/images',
@@ -14,6 +22,10 @@ export default defineConfig({
     }
   },
   server: {
+    https:{
+      key: 'cert/localhost-key.pem',
+      cert: 'cert/localhost.pem',
+    },
     proxy: {
       '/auth': {
         target: 'https://itpick.store',
