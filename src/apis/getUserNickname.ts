8b2cd1a8ac3894304ee5b97dce3036cd2c777fab
clice lee <cliceleee@gmail.com>
2024-08-18 17:42:54 +0900

    revert: API 익명 처리 revert

--- src/apis/getUserNickname.ts
+++ src/apis/getUserNickname.ts
@@ -4,9 +4,6 @@ export const getUserNickname = async () => {
   try {
     const token = localStorage.getItem('accessToken');
 
-    if(!token){
-      return '익명'; //로그인을 안 해서 토큰이 없으면 닉네임 익명 반환
-    }
     const response = await axios.get('/user/nickname', {
       headers: {
         'Authorization': `Bearer ${token}`,
@@ -16,6 +13,6 @@ export const getUserNickname = async () => {
     return response.data.result.nickname;
   } catch (error) {
     console.error('닉네임 불러오는 중 오류 발생:', error);
-    return '익명';
+    throw error;  
   }
 };
