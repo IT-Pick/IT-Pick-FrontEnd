eb8a3145d4b00a3c64491e9cbb5147107431f659
clice lee <cliceleee@gmail.com>
2024-08-20 07:13:39 +0900

    feat: 댓글작성  API 붙이는것만 완료 -post 테스트 안 해봄 -로컬에서도 안됨

--- src/pages/UploadedVotePage/components/CommentList.tsx
+++ src/pages/UploadedVotePage/components/CommentList.tsx
@@ -1,47 +1,30 @@
-import React, { useState } from "react";
-import { createComment } from "@apis/WriteDebate/createComment"; // createComment 함수가 위치한 파일 경로를 입력하세요.
+import React from "react";
+import Comment from "./Comment";
 
-interface CommentInputProps {
-    debateId: number;
-    parentCommentId?: number | null;
-    userId: number;
-    onCommentCreated: () => void; // 댓글 생성 후 댓글 목록을 갱신하기 위한 콜백 함수
+interface CommentListProps {
+    comments: {
+        userName: string;
+        time: number;
+        like: number;
+        text: string;
+    }[];
 }
 
-const CommentInput: React.FC<CommentInputProps> = ({ debateId, parentCommentId = null, userId, onCommentCreated }) => {
-    const [comment, setComment] = useState("");
-
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        if (!comment.trim()) return;
-
-        try {
-            await createComment({
-                comment,
-                parentCommentId,
-                debateId,
-                userId,
-            });
-            setComment("");
-            onCommentCreated(); // 부모 컴포넌트의 함수 호출로 댓글 목록을 갱신
-        } catch (error) {
-            console.error("댓글을 생성하는 중 오류가 발생했습니다.", error);
-        }
-    };
-
+const CommentList: React.FC<CommentListProps> = ({ comments} ) => {
     return (
-        <form onSubmit={handleSubmit} className="flex flex-col mb-4">
-            <textarea
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                placeholder="댓글을 입력하세요..."
-                className="border p-2 mb-2"
-            />
-            <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-                댓글 달기
-            </button>
-        </form>
+        <div className="flex flex-col border-t-[12px] border-[#EDF0F3] pt-[28px] pb-[42px]">
+            <div className="text-[14px] text-[#9EAAB5] ml-[28px] mb-[12px]">댓글 {comments.length}</div>
+            {comments.map((comment, index) => (
+                <Comment
+                    key={index}
+                    userName={comment.userName}
+                    time={comment.time}
+                    text={comment.text}
+                    like={comment.like}
+                />
+            ))}
+        </div>
     );
 };
 
-export default CommentInput;
+export default CommentList;
