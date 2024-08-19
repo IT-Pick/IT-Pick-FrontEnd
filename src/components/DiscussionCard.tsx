eab32a846a38bddd1db25328c541ca1120ae6290
clice lee <cliceleee@gmail.com>
2024-08-20 04:10:23 +0900

    Revert "error: LiveDiscussion 무한루프 에러"
    This reverts commit a6f78e2f8f30aca4152c98137fed66d507f9bb74.

--- src/components/DiscussionCard.tsx
+++ src/components/DiscussionCard.tsx
@@ -1,67 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import DiscussionCard from '@components/DiscussionCard';
-import { getLiveDiscussionBest3 } from '@apis/getLiveDiscussionBest3';
+import React from 'react';
+import tag_ico_view from '../assets/images/16x16/tag_ico_view.svg';
+import tag_ico_comment from '../assets/images/16x16/tag_ico_comment.svg';
 import PurpleBox from '@images/ico_purple_box.svg';
 
-interface Discussion {
-  image: string;
+interface DiscussionCardProps {
+  image: string | null;
   hits: number | null;
   comments: number | null;
   title: string;
   link: string;
+  className?: string; // 추가적인 클래스 이름을 위한 props
 }
 
-interface Response {
-  title: string;
-  debateId: number;
-  mediaUrl?: string;
-  hit: number;
-  comment: number;
-}
-
-const LiveDiscussion: React.FC = () => {
-  const [discussions, setDiscussions] = useState<Discussion[]>([]);
-
-  useEffect(() => {
-    const fetchDiscussions = async () => {
-      try {
-        const result: Response[] = await getLiveDiscussionBest3();
-        const formattedDiscussions: Discussion[] = result.map((item) => ({
-          title: item.title,
-          hits: item.hit,
-          image: item.mediaUrl || PurpleBox,
-          comments: item.comment,
-          link: `/debate/details?debateId=${item.debateId}`, // 링크를 debateId로 초기화
-        }));
-        setDiscussions(formattedDiscussions);
-      } catch (error) {
-        console.error('실시간 토론 데이터를 가져오는 중 오류 발생:', error);
-      }
-    };
-
-    fetchDiscussions();
-  }, []);
-
-  return (
-    <div className="mt-[26px] ml-[24px]">
-      <div className="flex flex-shrink-0">
-        <span className="text-[#2E333B] font-pretendard text-[20px] font-bold leading-normal">실시간 토론 BEST 3</span>
-      </div>
-      <div className="flex flex-row space-x-[12px] mt-[12px] overflow-x-scroll overflow-y-hidden scrollbar-hidden w-auto h-[100%] pr-[24px]">
-        {discussions.map((discussion, index) => (
-          <DiscussionCard 
-            key={index}
-            title={discussion.title}
-            image={discussion.image}
-            hits={discussion.hits}
-            comments={discussion.comments}
-            link={discussion.link}
-            className="w-[164px] h-[100%] flex-shrink-0"
-          />
-        ))}
-      </div>
-    </div>
-  );
+const formatNumber = (num: number | null) => {
+  return num !== null ? new Intl.NumberFormat().format(num) : '';
 };
 
-export default LiveDiscussion;
+const DiscussionCard: React.FC<DiscussionCardProps> = ({ image, hits, comments, title, link, className }) => (
+  <div className={`font-pretendard ${className}`}>
+    {image === PurpleBox ? (
+      <div 
+        className="w-[164px] h-[200px] bg-point500 rounded-2xl mb-2 flex items-center justify-center text-white text-center p-2"
+        style={{ backgroundImage: `url(${PurpleBox})`, backgroundSize: 'cover' }}
+      >
+        <span className="text-[14px] font-pretendard font-semibold px-[15px] leading-6">{title}</span>
+    </div>
+  ) : (
+      <img src={image} alt={title} className="w-[164px] h-[200px] rounded-2xl mb-2 object-cover object-center" />
+    )}
+    <div className="flex justify-start items-center gap-2 mb-2">
+      {hits !== null && (
+        <div className="bg-point100 text-point500 rounded-2xl px-2 py-1 gap-[2px] flex items-center font-pretendard font-medium text-[12px]">
+          <img src={tag_ico_view} alt="tag_ico_view" />
+          {formatNumber(hits)}
+        </div>
+      )}
+      {comments !== null && (
+        <div className="bg-point100 text-point500 rounded-2xl px-2 py-1 gap-[2px] flex items-center font-pretendard font-medium text-[12px]">
+          <img src={tag_ico_comment} alt="tag_ico_comment" />
+          {formatNumber(comments)}
+        </div>
+      )}
+    </div>
+    <a href={link} className="text-black font-bold text-[16px] text-left">
+      {title}
+    </a>
+  </div>
+);
+
+export default DiscussionCard;
