4935a5ce27c6d554e9585b651c1a6effa76bbac1
yskim6772 <yskim6772@gmail.com>
2024-08-19 22:26:19 +0900

    Chore: intermediate commit

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -5,56 +5,8 @@ import PopularSearches from './components/PopularSearches';
 import RecentDiscussions from './components/RecentDiscussions';
 import SearchResultItem from './components/SearchResultItem';
 import ErrorPage from './components/405ErrorPage';
-// import LiveDiscussion1 from '../../assets/images/LiveDiscussion/LiveDiscussion1.png';
-// import LiveDiscussion2 from '../../assets/images/LiveDiscussion/LiveDiscussion2.png';
-// import LiveDiscussion3 from '../../assets/images/LiveDiscussion/LiveDiscussion3.png';
-import PurpleBox from '../../assets/images/ico_purple_box.svg';
+// import PurpleBox from '../../assets/images/ico_purple_box.svg';
 import { getKeyword } from '@apis/getKeyword';
-
-const discussions = [
-  {
-    image: PurpleBox,
-    hits: 1210,
-    comments: 2830,
-    title: "김현주 열애설 어떻게 생각함?",
-    link: "/Post1",
-  },
-  {
-    image: PurpleBox,
-    hits: 990,
-    comments: null,
-    title: "김현주가 아깝다 vs 차은우가 아깝다",
-    link: "/Post2",
-  },
-  {
-    image: PurpleBox,
-    hits: 32,
-    comments: 48302,
-    title: "현주씨 오늘 저녁 뭐 드셨어요",
-    link: "/Post3",
-  },
-  {
-    image: PurpleBox,
-    hits: 93021,
-    comments: 123,
-    title: "뉴진스! 뉴진스!",
-    link: "/Post4",
-  },
-  {
-    image: PurpleBox,
-    hits: null,
-    comments: 594,
-    title: "현주씨 오늘 저녁 뭐 드셨어요",
-    link: "/Post5",
-  },
-  {
-    image: PurpleBox,
-    hits: 145,
-    comments: 3048,
-    title: "김현주 열애설 어떻게 생각함?",
-    link: "/Post6",
-  },
-];
 
 const SearchPage: React.FC = () => {
   const [tags, setTags] = useState<string[]>([]);
