5719cffa9a205ca8d1c70c9e260710826411443d
yskim6772 <yskim6772@gmail.com>
2024-08-19 16:05:19 +0900

    Chore: revert popular searches ranking order

--- src/pages/SearchPage/components/PopularSearches.tsx
+++ src/pages/SearchPage/components/PopularSearches.tsx
@@ -24,13 +24,14 @@ const PopularSearches: React.FC = () => {
     try {
       const rankingData = await getRankingInfo('total', 'real_time', '');
       const top8Searches = rankingData.slice(0, 8).map(item => item.name); // 상위 8개의 name 가져오기
-      setSearches(rearrangeSearches(top8Searches));
+      setSearches(top8Searches);
     } catch (error) {
       console.error('인기 검색어 데이터를 불러오는 중 오류 발생:', error);
       setSearches([]);
     }
   };
 
+  // maxLength 이상이면 ... 처리
   const truncateText = (text: string, maxLength: number) => {
     if (text.length > maxLength) {
       return text.slice(0, maxLength) + '...';
@@ -38,21 +39,7 @@ const PopularSearches: React.FC = () => {
     return text;
   };
 
-  const rearrangeSearches = (searches: string[]) => {
-    const halfLength = Math.ceil(searches.length / 2);
-    const firstColumn = searches.slice(0, halfLength);
-    const secondColumn = searches.slice(halfLength);
-
-    const rearranged = [];
-    for (let i = 0; i < halfLength; i++) {
-      rearranged.push(firstColumn[i]);
-      if (secondColumn[i]) {
-        rearranged.push(secondColumn[i]);
-      }
-    }
-
-    return rearranged;
-  };
+  const halfLength = Math.ceil(searches.length / 2);
 
   return (
     <div className="mt-8 mb-4">
@@ -63,14 +50,16 @@ const PopularSearches: React.FC = () => {
         </span>
       </div>
       <div className="grid grid-cols-2 gap-x-8 ml-8 mr-8">
-        {searches.map((search, index) => (
+        {searches.slice(0, halfLength).map((search, index) => (
           <div key={index} className="flex items-center mt-3">
-            <span className="font-pretendard font-bold text-[14px] text-gray3 mr-4">
-              {index + 1}
-            </span>
-            <span className="font-pretendard font-semibold text-[14px] text-black">
-              {truncateText(search, 10)}
-            </span>
+            <span className="font-pretendard font-bold text-[14px] text-gray3 mr-4">{index + 1}</span>
+            <span className="font-pretendard font-semibold text-[14px] text-black">{truncateText(search, 10)}</span>
+          </div>
+        ))}
+        {searches.slice(halfLength).map((search, index) => (
+          <div key={index + halfLength} className="flex items-center mt-3">
+            <span className="font-pretendard font-bold text-[14px] text-gray3 mr-4">{index + halfLength + 1}</span>
+            <span className="font-pretendard font-semibold text-[14px] text-black">{truncateText(search, 10)}</span>
           </div>
         ))}
       </div>
