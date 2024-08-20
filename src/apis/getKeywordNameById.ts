4e0c06dcd749728ffece1180bcb23ef0693366f9
clice lee <cliceleee@gmail.com>
2024-08-20 14:38:57 +0900

    remove: 존재하지 않는 API 삭제 -백엔드가 만들어주면 그떄 다시 생성

--- src/apis/getKeywordNameById.ts
+++
@@ -1,7 +0,0 @@
-// @apis/Keywords/getKeywordNameById.ts
-import axios from 'axios';
-
-export const getKeywordNameById = async (keywordId: number): Promise<string> => {
-  const response = await axios.get(`/api/keywords/${keywordId}`);
-  return response.data.keywordName;
-};
