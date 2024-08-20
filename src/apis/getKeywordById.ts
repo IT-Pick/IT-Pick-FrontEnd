1ac2c4359e4f82ffb4899134c4f1de0883555c57
clice lee <cliceleee@gmail.com>
2024-08-20 15:38:23 +0900

    feat: api

---
+++ src/apis/getKeywordById.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// 키워드 ID로 키워드 이름을 불러오는 함수
+export const getKeywordById = async (keywordId: number) => {
+    const response = await axios.get(`/keyword/search`, {
+        params: {
+            query: keywordId.toString(),
+        },
+    });
+    return response.data; // 필요한 키워드 이름 데이터를 반환
+};
